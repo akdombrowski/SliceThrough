8f88c9b9d4e964fc8e3cd35cc08da48ea26c50cb
akdombrowski <akdflyinvee73@gmail.com>
2021-08-21 07:27:49 -0500

    v1.0.0 for e1.6.0

--- Readme.md
+++ Readme.md
@@ -1,15 +1,15 @@
-﻿SliceThrough mod
+﻿# SliceThrough mod
 
-Update to the old great Xorberax's Cut Through mod.
+### Update to the old great Xorberax's Cut Through mod.
 
-Enables you to slice through multiple enemies.
+## Enables you to slice through multiple enemies.
 
 *It won't slice through if the attack is blocked (e.g., by shield or weapon parry).
 
-Don't forget to endorse on Nexus mods or through Vortex!
+# Don't forget to endorse on Nexus mods or through Vortex!
 
 
-Helpful information for modders:
+#### Helpful information for modders:
 
 (CTRL+Left Click on link to open in your browser)
 Bannerlord Modding Documentation:   https://docs.bannerlordmodding.com/
--- SubModule.cs
+++ SubModule.cs
@@ -1,5 +1,8 @@
 ﻿using HarmonyLib;
 
+using System;
+using System.Collections.Generic;
+
 using TaleWorlds.Core;
 using TaleWorlds.Engine;
 using TaleWorlds.Library;
@@ -7,67 +10,55 @@ using TaleWorlds.MountAndBlade;
 
 namespace SliceThrough
 {
-  public class SubModule : MBSubModuleBase
-  {
-    [HarmonyPostfix]
-    [HarmonyPatch(typeof(Mission), "DecideWeaponCollisionReaction")]
-    protected static void DecideWeaponCollisionReaction(
-      Blow registeredBlow,
-      in AttackCollisionData collisionData,
-      Agent attacker,
-      Agent defender,
-      in MissionWeapon attackerWeapon,
-      bool isFatalHit,
-      bool isShruggedOff,
-      out MeleeCollisionReaction colReaction, Mission __instance)
+    [HarmonyPatch]
+    public class SubModule : MBSubModuleBase
     {
-      Harmony.DEBUG = false;
-      Debug.Print("");
-      FileLog.Log("");
+        private static bool DEBUG = false;
 
-      // original code
-      if (collisionData.IsColliderAgent && collisionData.StrikeType == 1 && collisionData.CollisionHitResultFlags.HasAnyFlag<CombatHitResultFlags>(CombatHitResultFlags.HitWithStartOfTheAnimation))
-        colReaction = MeleeCollisionReaction.Staggered;
-      else if (!collisionData.IsColliderAgent && collisionData.PhysicsMaterialIndex != -1 && PhysicsMaterial.GetFromIndex(collisionData.PhysicsMaterialIndex).GetFlags().HasAnyFlag<PhysicsMaterialFlags>(PhysicsMaterialFlags.AttacksCanPassThrough))
-        colReaction = MeleeCollisionReaction.SlicedThrough;
-      else if (!collisionData.IsColliderAgent || registeredBlow.InflictedDamage <= 0)
-        colReaction = MeleeCollisionReaction.Bounced;
-      else if (collisionData.StrikeType == 1 && attacker.IsDoingPassiveAttack)
-        colReaction = MissionGameModels.Current.AgentApplyDamageModel.DecidePassiveAttackCollisionReaction(attacker, defender, isFatalHit);
-      else
-      {
-        WeaponClass weaponClass = !attackerWeapon.IsEmpty ? attackerWeapon.CurrentUsageItem.WeaponClass : WeaponClass.Undefined;
-        int num1 = attackerWeapon.IsEmpty ? 0 : (!isFatalHit ? 1 : 0);
-        int num2 = isShruggedOff ? 1 : 0;
-        colReaction = (num1 & num2) != 0 || attackerWeapon.IsEmpty && defender != null && defender.IsHuman && !collisionData.IsAlternativeAttack && (collisionData.VictimHitBodyPart == BoneBodyPartType.Chest || collisionData.VictimHitBodyPart == BoneBodyPartType.ShoulderLeft || (collisionData.VictimHitBodyPart == BoneBodyPartType.ShoulderRight || collisionData.VictimHitBodyPart == BoneBodyPartType.Abdomen) || collisionData.VictimHitBodyPart == BoneBodyPartType.Legs) ? MeleeCollisionReaction.Bounced : ((weaponClass == WeaponClass.OneHandedAxe || weaponClass == WeaponClass.TwoHandedAxe) && (!isFatalHit && (double)collisionData.InflictedDamage < (double)defender.HealthLimit * 0.5) || attackerWeapon.IsEmpty && !collisionData.IsAlternativeAttack && collisionData.AttackDirection == Agent.UsageDirection.AttackUp || collisionData.ThrustTipHit && (sbyte)collisionData.DamageType == (sbyte)1 && (!attackerWeapon.IsEmpty && defender.CanThrustAttackStickToBone(collisionData.CollisionBoneIndex)) ? MeleeCollisionReaction.Stuck : MeleeCollisionReaction.SlicedThrough);
-        if (!collisionData.AttackBlockedWithShield && !collisionData.CollidedWithShieldOnBack || colReaction != MeleeCollisionReaction.SlicedThrough)
-          return;
-        colReaction = MeleeCollisionReaction.Bounced;
-      }
-      // original code
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Mission), "UpdateMomentumRemaining")]
+        private static void UpdateMomentumRemaining(
+            ref float momentumRemaining,
+            Blow b,
+            in AttackCollisionData collisionData,
+            Agent attacker,
+            Agent victim,
+            in MissionWeapon attackerWeapon,
+            ref bool isCrushThrough)
+        {
 
-      if (attacker.IsMine)
-      {
-        Debug.Print("attacker: " + attacker.Name);
-        FileLog.Log("attacker: " + attacker.Name);
-        Debug.Print("Collision Reaction: SlicedThrough");
-        FileLog.Log("Collision Reaction: SlicedThrough");
-        colReaction = MeleeCollisionReaction.SlicedThrough;
-      }
+            if (attacker.IsHero || attacker.IsMainAgent || attacker.IsPlayerControlled)
+            {
+                Harmony.DEBUG = DEBUG;
+                List<string> buffer = FileLog.GetBuffer(true);
 
-      Debug.Print("");
-      FileLog.Log("");
-      Harmony.DEBUG = false;
+                buffer.Add("");
+                buffer.Add("");
+                buffer.Add("UpdateMomentumRemaining");
+                buffer.Add("attacker.IsHero: " + attacker.IsHero);
+                buffer.Add("attacker.Name: " + attacker.Name);
+                buffer.Add("isCrushThrough before: " + isCrushThrough);
+                buffer.Add("momentumRemaining before: " + momentumRemaining);
+                isCrushThrough = true;
+                momentumRemaining = 1000;
+                buffer.Add("isCrushThrough after: " + isCrushThrough);
+                buffer.Add("momentumRemaining after: " + momentumRemaining);
+
+                FileLog.LogBuffered(buffer);
+                FileLog.FlushBuffer();
+
+                Harmony.DEBUG = false;
+            }
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+            var harmony = new Harmony("com.SliceThrough.akdombrowski");
+
+            harmony.PatchAll();
+
+            InformationManager.DisplayMessage(new InformationMessage("Loaded 'SliceThrough'.", Color.FromUint(0182999992U)));
+        }
     }
-
-    protected override void OnBeforeInitialModuleScreenSetAsRoot()
-    {
-      base.OnBeforeInitialModuleScreenSetAsRoot();
-      var harmony = new Harmony("com.DontInterruptMe.akdombrowski");
-
-      harmony.PatchAll();
-
-      InformationManager.DisplayMessage(new InformationMessage("Loaded 'AttackInteruptModifier'.", Color.FromUint(0182999992U)));
-    }
-  }
 }
